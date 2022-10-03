4e9d7642bca15a034f8f19a76d647dec92a69330
MuhireIghor <muhireighor123@gmail.com>
2022-10-03 11:01:43 +0200

    Initial commit

---
+++ src/BankSystem.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ class userAgent{
+    String name;
+    String accId;
+    int amount;
+    void register(int capital,String userInfo){
+        accId = userInfo;
+        amount = capital;
+
+        System.out.Println("User "+name+" with account number ")
+    }
+}
